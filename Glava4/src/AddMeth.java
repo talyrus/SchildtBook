2efc2a7611ef43f1de620b023da2ee416b9fc5d0
Taly <talyrus@gmail.com>
2016-12-22 13:39:29 +0300

    Задания по книге Шилдт Java 8 "Руководство для начинающих" Глава 4

---
+++ Glava4/src/AddMeth.java
@@ -0,0 +1,40 @@
+/**
+ * Created by Taly on 22.12.2016.
+ * добавление метода range() в класс Vehicle
+ */
+
+class Vehicle {
+    int passengers; // количество пассажиров
+    int fuelcap;    // емкость топливного бака
+    int mpg;        // потребление топлива в милях на галлон
+
+    void range(){
+        System.out.println("Дальность - " + fuelcap*mpg + " миль.");
+    }
+}
+
+class AddMeth {
+    public static void main(String[] args) {
+
+        Vehicle minivan = new Vehicle();
+        Vehicle sportscar = new Vehicle(); // переменные minivan и sportscar ссылаются на разные объекты
+
+        int range1, range2;
+
+        //Присвоить значение полям в объекте minivan
+        minivan.passengers = 7;
+        minivan.fuelcap = 16;
+        minivan.mpg = 21;
+
+        //Присвоить значение полям в объекте sportscar
+        sportscar.passengers = 2;
+        sportscar.fuelcap = 14;
+        sportscar.mpg = 12;
+
+        System.out.print("Мини-фургон может перевезти " + minivan.passengers + " пассажиров.");
+        minivan.range();
+
+        System.out.println("Спортивный автомобиль может перевезти " + sportscar.passengers + " пассажиров.");
+        sportscar.range();
+    }
+}
