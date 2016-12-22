ac1696f8e8d26729438881581a6391ea4aa7c0ee
Taly <talyrus@gmail.com>
2016-12-22 11:52:59 +0300

    Задания по книге Шилдт Java 8 "Руководство для начинающих" Глава 4

---
+++ Glava4/src/VehicleDemo.java
@@ -0,0 +1,28 @@
+/**
+ * Created by Taly on 21.12.2016.
+ */
+class Vehicle {
+    int passengers; // количество пассажиров
+    int fuelcap;    // емкость топливного бака
+    int mpg;        // потребление топлива в милях на галлон
+}
+
+
+    //В этом классе объявляется объект типа Vehicle
+    class VehicleDemo {
+    public static void main(String[] args) {
+        Vehicle minivan = new Vehicle();
+        int range;
+
+        //Присвоить значения полям в объекте minivan
+        minivan.passengers = 7;
+        minivan.fuelcap = 16;
+        minivan.mpg = 21;
+
+        //рассчитать дальность поездки при полном баке
+        range = minivan.fuelcap * minivan.mpg;
+        System.out.println("Мини-фургон может перевезти " + minivan.passengers + " пассажиров на расстояние "
+                + range + " миль");
+    }
+}
+
