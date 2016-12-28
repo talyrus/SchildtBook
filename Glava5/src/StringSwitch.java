f670119415419b71f569e6170af3c321fcff0881
Taly <talyrus@gmail.com>
2016-12-28 23:06:00 +0300

    Задания по книге Шилдт Java 8 "Руководство для начинающих" Глава 5

---
+++ Glava5/src/StringSwitch.java
@@ -0,0 +1,25 @@
+/**
+ * Created by Taly on 28.12.2016.
+ *  Использование строк для управления оператором switch
+ */
+class StringSwitch {
+    public static void main(String[] args) {
+        String command = "cancel";
+
+        switch (command){
+            case "connect":
+                System.out.println("Connect");
+                break;
+            case "cancel":
+                System.out.println("Cancel");
+                break;
+            case "disconnect":
+                System.out.println("Disconnect");
+                break;
+            default:
+                System.out.println("Wrong command!");
+                break;
+        }
+    }
+
+}
