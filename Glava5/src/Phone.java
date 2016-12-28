cab11404f3a770f37564d48b7967f81d93151c8c
Taly <talyrus@gmail.com>
2016-12-28 23:30:36 +0300

    Задания по книге Шилдт Java 8 "Руководство для начинающих" Глава 5

---
+++ Glava5/src/Phone.java
@@ -0,0 +1,29 @@
+/**
+ * Created by Taly on 28.12.2016.
+ *  Простейший автоматизированный телефонный справочник
+ */
+class Phone {
+    public static void main(String[] args) {
+        String numbers[][] = {
+                {"Tom", "555-3322"},
+                {"Mary", "555-8976"},
+                {"Jon", "555-1037"},
+                {"Rachel", "555-1400"}
+        };
+        int i;
+        //Для того, чтобы воспользоваться программой, ей нужно передать один
+        //аргумент командной строки
+        if (args.length != 1)
+            System.out.println("Использование: java Phone <имя>");
+        else {
+            for (i = 0; i < numbers.length; i++){
+                if (numbers[i][0].equals(args[0])) {
+                    System.out.println(numbers[i][0] + ": " + numbers[i][1]);
+                    break;
+                }
+            }
+            if (i == numbers.length)
+                System.out.println("Имя не найдено.");
+        }
+    }
+}
