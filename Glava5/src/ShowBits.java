56ac08b6934fb1bccb41251787a4fbaaaad62354
Taly <talyrus@gmail.com>
2016-12-29 23:40:48 +0300

    Задания по книге Шилдт Java 8 "Руководство для начинающих" Глава 5

---
+++ Glava5/src/ShowBits.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Taly on 29.12.2016.
+ * Отображение битов, составляющих байт
+ */
+class ShowBits {
+    public static void main(String[] args) {
+        int t;
+        byte val;
+
+        val = 123;
+        for (t = 128; t > 0 ; t = t / 2) {
+            if ((val & t) != 0) System.out.print("1 ");
+            else System.out.print("0 ");
+        }
+    }
+}
