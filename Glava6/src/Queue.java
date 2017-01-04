d7a97ee7c204fb151b1e73345b0fb8dae67514a5
Taly <talyrus@gmail.com>
2017-01-04 11:56:09 +0300

    Задания по книге Шилдт Java 8 "Руководство для начинающих" Глава 6 *Усовершенствованный класс очереди, предназначенной для хранения символьных значений

---
+++ Glava6/src/Queue.java
@@ -0,0 +1,76 @@
+/**
+ * Created by Taly on 04.01.2017.
+ * Усовершенствованный класс очереди, предназначенной для
+ * хранения символьных значений
+ */
+class Queue {
+    //Эти члены класса теперь являются закрытыми
+    private char q[]; //массив для хранения элементов очереди
+    private int putloc, getloc; // индексы для вставки и извлечения элементов очереди
+
+    Queue(int size){ //конструктор класса Queue создает очередь заданного размера
+        q = new char[size + 1]; //выделить память для очереди
+        putloc = getloc = 0;
+    }
+
+    //Поместить символ в очередь
+    void put(char ch) {
+        if (putloc == q.length - 1) {
+            System.out.println(" - Очередь заполнена");
+            return;
+        }
+        putloc++;
+        q[putloc] = ch;
+    }
+
+    //Извлечь символ из очереди
+    char get(){
+        if (getloc == putloc) {
+            System.out.println(" - Очередь пуста");
+            return (char) 0;
+        }
+        getloc++;
+        return q[getloc];
+    }
+}
+
+//Демонстрация использования класса Queue
+class QDemo {
+    public static void main(String[] args) {
+        Queue bigQ = new Queue(100);
+        Queue smallQ = new Queue(4);
+        char ch;
+        int i;
+
+        System.out.println("Использование очереди bigQ для сохранения алфавита");
+        //Поместить буквенные символы в очередь bigQ
+        for (i = 0; i < 26; i++)
+            bigQ.put((char)('A' + i));
+
+        //Извлечь буквенные символы из очереди bigQ и отобразить
+        System.out.print("Содержимое очереди bigQ: ");
+        for (i = 0; i < 26; i++) {
+            ch = bigQ.get();
+            if (ch != (char) 0) System.out.print(ch);
+        }
+
+        System.out.println("\n");
+        System.out.println("Использование очереди smallQ для генерации ошибок");
+        //Использование очереди smallQ для генерации ошибок
+        for (i = 0; i < 5; i++) {
+            System.out.print("Попытка сохранения " + (char) ('Z' - i));
+            smallQ.put((char) ('Z' - i));
+            System.out.println();
+        }
+        System.out.println();
+
+        //Дополнительные ошибки при обращении к очереди smallQ
+        System.out.print("Содержимое smallQ: ");
+        for (i = 0; i < 5; i++) {
+            ch = smallQ.get();
+            if (ch != (char) 0) System.out.print(ch);
+        }
+    }
+
+}
+
